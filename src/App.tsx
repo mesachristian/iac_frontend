c396bc3a5ff6782250a80adb20b9ff80104f80bc
Christian Mesa <mesachristian_coop@outlook.com>
2022-08-25 20:33:43 -0500

    First IAC Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const [translate] = useTranslation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline text-red-600">
+      {translate("global.lblHelloWorld")}
+    </h1>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Translate service
+import i18next from 'i18next';
+import { I18nextProvider, initReactI18next  } from 'react-i18next';
+import global_es from './assets/i18n/es/global.json';
+import global_en from './assets/i18n/en/global.json';
+
+i18next
+  .use(initReactI18next)
+  .init({
+  interpolation : { escapeValue : false },
+  lng : "es",
+  resources : {
+    es : {
+      translation : global_es
+    },
+    en : {
+      translation : global_en
+    },
+  }
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
